a6fc15066f6dfe93eec8e35ff321b03b3f3a09c5
domagojkristov <domagojkk@gmail.com>
2020-08-25 19:20:24 +0200

    Updates

---
+++ precache-manifest.6d0b38936cbdbceeb2796c40495dad5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb3b3633b56be55f3e842552373bcd4",
+    "url": "/MBClient/index.html"
+  },
+  {
+    "revision": "d0f37fb09c7f1a0775b9",
+    "url": "/MBClient/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b2a3ffc41a89d3f64bf4",
+    "url": "/MBClient/static/css/main.faa9987e.chunk.css"
+  },
+  {
+    "revision": "d0f37fb09c7f1a0775b9",
+    "url": "/MBClient/static/js/2.05bbb3d9.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/MBClient/static/js/2.05bbb3d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2a3ffc41a89d3f64bf4",
+    "url": "/MBClient/static/js/main.7617efbb.chunk.js"
+  },
+  {
+    "revision": "639f0c7d56b7bfcd6a3f",
+    "url": "/MBClient/static/js/runtime-main.339d427a.js"
+  }
+]);
